0508108c01a29d00a5a491437c0c83133f442b07
Peter Elmers <peter.elmers@yahoo.com>
2014-12-05 09:31:45 -0600

    it's a quine!

---
+++ scrip.py
@@ -0,0 +1,14 @@
+# Print array of subsequent character ordinal offsets in string s
+s = r'''    print!("fn main() {{\n    let p:[i8,..180] = {};\n", p.as_slice())
+    let mut c = 0;
+    for x in p.iter() {
+        c += *x;
+        print!("{}", (c as u8) as char);
+    }
+}
+'''
+arr = [ord(s[0])]
+for p,n in zip(s, s[1:]):
+    arr.append(ord(n) - ord(p))
+print(arr)
+print(len(arr))
